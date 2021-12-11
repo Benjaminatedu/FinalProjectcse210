f66f4545e5bd1c195ae0bc17f5d076f4d4aef0ac
Benjaminatedu <91627659+Benjaminatedu@users.noreply.github.com>
2021-12-11 13:49:03 -0700

    Projectile image added

--- Casting/Projectile.cs
+++ Casting/Projectile.cs
@@ -4,6 +4,7 @@ namespace cse210_batter_csharp.Casting
     {
         public Projectile(int x, int y, int xVel)
         {
+            SetImage(Constants.PROJECTILE_IMAGE);
             SetPosition(new Point(x,y));
             SetHeight(10);
             SetWidth(10);
